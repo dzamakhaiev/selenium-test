855cd7d3f8c256b39b055934162f70a59a7d9690
dzamakhaiev <dzamakhaiev@star.global>
2024-06-14 11:00:10 +0300

    add test data

--- test_data.py
+++ test_data.py
@@ -1,3 +1,5 @@
 class MainPage:
 
+    EXP_URL = 'https://demoqa.com/'
     EXP_TITLE = 'DEMOQA'
+    EXP_CARDS = ['Elements', 'Forms', 'Alerts, Frame & Windows', 'Widgets', 'Interactions', 'Book Store Application']
