4341c355e8df5c25a53453e5039ce1ce7aefe855
dzamakhaiev <dzamakhaiev@star.global>
2024-06-14 11:00:24 +0300

    add test

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -7,8 +7,17 @@ def test_main_page(driver):
     main_page = pages.MainPage(driver)
     main_page.go_to_main_page()
 
-    assert main_page.url == main_page.get_current_url()
+    assert main_page.get_current_url() == test_data.MainPage.EXP_URL
     assert main_page.get_page_title() == test_data.MainPage.EXP_TITLE
+
+
+def test_main_page_elements(driver):
+    main_page = pages.MainPage(driver)
+    main_page.go_to_main_page()
+
+    elements = main_page.get_list_of_cards()
+    cards = [element.text for element in elements]
+    assert cards == test_data.MainPage.EXP_CARDS
 
 
 if __name__ == '__main__':
